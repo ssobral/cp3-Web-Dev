44e3ecb33a68442c18cccf45c7a387365fc4a865
Murilo Mansano <murilomansano2@gmail.com>
2023-05-09 19:31:59 -0300

    Terceiro commit - Murilo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function trocar(cor){
     document.body.style.background = cor
 }
+
+
+const data = new Date()
+
+const dia = String(data.getDate()).padStart(2,0)
+
+const mes = String(data.getMonth()).padStart(2,0)
+
+const ano = data.getFullYear()
+
+const dataAtual = `${dia}/${mes}/${ano}`
